b6ba90ec5049f7b382b8a1640fc5e16de7ae040e
Pierre Silva <pierremichelsilva@gmail.com>
2019-02-02 13:11:12 -0500

    some changes

--- packages/pierresilva/laravel-schema-builder/src/schema-designer/src/components/Modals/ForeignKeyForm.js
+++ packages/pierresilva/laravel-schema-builder/src/schema-designer/src/components/Modals/ForeignKeyForm.js
@@ -130,7 +130,6 @@ class ForeignKeyForm extends PureComponent<Props, State> {
     }
 
     setCurrentMethodName = (event: { target: { value: string } }) => {
-
         this.setState({
             currentMethodName: event.target.value
         });
